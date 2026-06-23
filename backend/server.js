19383fb55772683e73ca1486db108b830639e830
Vijay Solanki <vijaysolankia0@gmail.com>
2026-06-23 15:46:35 +0530

    Update server js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const connectDB = require("./config/db");
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/projects", require("./routes/projectRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
 
 app.get("/", (req, res) => {
   res.json({
