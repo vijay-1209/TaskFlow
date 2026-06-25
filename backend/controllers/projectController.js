9cdc014e413cc059095e21b80acba0806292d8fd
Vijay Solanki <vijaysolankia0@gmail.com>
2026-06-25 14:44:36 +0530

    created a project controller

---
+++ backend/controllers/projectController.js
@@ -0,0 +1,44 @@
+const Project = require("../models/Project");
+
+// Create Project
+exports.createProject = async (req, res) => {
+  try {
+    const project = await Project.create(req.body);
+
+    res.status(201).json(project);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// Get All Projects
+exports.getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find();
+
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// Delete Project
+exports.deleteProject = async (req, res) => {
+  try {
+    await Project.findByIdAndDelete(
+      req.params.id
+    );
+
+    res.status(200).json({
+      message: "Project deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
